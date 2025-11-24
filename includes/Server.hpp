6ba6e791c7b33abaef6f3f9b53561dad3540191d
Hassan Kheireddin <hassankheireddin0@gmail.com>
2025-11-25 00:21:03 +0200

    finish implement server file

--- includes/Server.hpp
+++ includes/Server.hpp
@@ -7,8 +7,15 @@
 #include <map>
 #include <poll.h>
 #include "Messages.hpp"
-#include "Users.hpp"
+#include "Clients.hpp"
 #include "Channel.hpp"
+#include <cstring>
+#include <unistd.h>
+#include <fcntl.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include "Command.hpp"
 
 using namespace std;
 
@@ -19,14 +26,14 @@ class Server
         string _password;
         int _serverSocket;
         vector<pollfd> _pollFds;
-        map<int, Users*> _users;
+        map<int, Clients*> _Clients;
         map<string, Channel*> _channels;
         Messages _messages;
-        map<string, Users*> _registeredNicknames;
+        map<string, Clients*> _registeredNicknames;
 
         void setupServerSocket();
         void acceptNewConnection();
-        void handleUserData(int userSocket);
+        void handleClientData(int ClientSocket);
 
     public:
         Server(int port, const string& password);
@@ -34,14 +41,12 @@ class Server
         
         void run();
 
-        bool isNicknameRegistered(const string& nickname) const;
-        void registerNickname(const string& nickname, Users* user);
-        void unregisterNickname(const string& nickname);
+        bool manageNickname(const std::string &nickname, Client* client, NicknameOperation op);
 
         const string& getPassword() const;
         Channel* createOrGetChannel(const string& channelName);
-        const map<string, Channel*>& getUser() const;
-        Users* getUserByNickname(const string& nickname) const;
+        const map<string, Channel*>& getClient() const;
+        Clients* getClientByNickname(const string& nickname) const;
         
 };
 
--- srcs/Server.cpp
+++ srcs/Server.cpp
@@ -44,6 +44,115 @@ void Server::setupServerSocket()
     std::cout << "Server is listening on port " << _port << "\n";
 }
 
+void Server::run() {
+    while (true) {
+        int ret = poll(&_pollfds[0], _pollfds.size(), -1);
+        if (ret < 0)
+            throw std::runtime_error("Poll failed");
+
+        for (size_t i = 0; i < _pollfds.size(); ++i) {
+            if (_pollfds[i].revents & POLLIN) {
+                if (_pollfds[i].fd == _serverSocket) {
+                    acceptNewConnection();
+                } else {
+                    handleClientData(_pollfds[i].fd);
+                }
+            }
+        }
+    }
+}
+
+void Server::acceptNewConnection() {
+    sockaddr_in clientAddr;
+    socklen_t len = sizeof(clientAddr);
+    int clientFd = accept(_serverSocket, (struct sockaddr *)&clientAddr, &len);
+    if (clientFd < 0)
+        return;
+
+    fcntl(clientFd, F_SETFL, O_NONBLOCK);
+
+    pollfd clientPoll;
+    clientPoll.fd = clientFd;
+    clientPoll.events = POLLIN;
+    _pollfds.push_back(clientPoll);
+
+    std::string ip = inet_ntoa(clientAddr.sin_addr);
+    _clients[clientFd] = new Client(clientFd, ip);
+
+    std::cout << "New client connected: " << clientFd << " (IP: " << ip << ")\n";
+}
+
+void Server::handleClientData(int clientFd) {
+    char buffer[1024];
+    std::memset(buffer, 0, sizeof(buffer));
+    ssize_t bytes = recv(clientFd, buffer, sizeof(buffer), 0);
+
+    if (bytes <= 0) {
+        std::cout << "Client disconnected: " << clientFd << "\n";
+    
+        // Cleanup nickname if set
+        Client* client = _clients[clientFd];
+        std::string nick = client->getNickname();
+        if (!nick.empty())
+            unregisterNickname(nick);
+    
+        // Close socket
+        close(clientFd);
+    
+        // Remove from poll list
+        for (size_t i = 0; i < _pollfds.size(); ++i) {
+            if (_pollfds[i].fd == clientFd) {
+                _pollfds.erase(_pollfds.begin() + i);
+                break;
+            }
+        }
+    
+        // Delete client
+        delete client;
+        _clients.erase(clientFd);
+        return;
+    }
+    
+
+    Client* client = _clients[clientFd];
+    client->appendToBuffer(std::string(buffer, bytes));
+
+    std::string& fullBuffer = client->getBuffer();
+    size_t pos;
+    while ((pos = fullBuffer.find("\n")) != std::string::npos) {
+        std::string commandLine = fullBuffer.substr(0, pos);
+        if (!commandLine.empty() && commandLine[commandLine.length() - 1] == '\r')
+            commandLine = commandLine.substr(0, commandLine.length() - 1);
+        fullBuffer.erase(0, pos + 1);
+    
+        std::string displayName = client->getNickname().empty() ? "(unknown)" : client->getNickname();
+        std::cout << "Parsing command from client " << clientFd << " (" << displayName << "): " << commandLine << "\n";
+        
+        Command::executeCommand(commandLine, client, *this);
+    }
+    
+}
+
+bool Server::manageNickname(const std::string &nickname, Client* client, NicknameOperation op) {
+    switch (op) {
+        case CHECK:
+            return _nickToClient.find(nickname) != _nickToClient.end();
+        case REGISTER:
+            _nickToClient[nickname] = client;
+            return true;
+        case UNREGISTER:
+            _nickToClient.erase(nickname);
+            return true;
+        default:
+            return false;
+    }
+}
+
+// Usage:
+// if (server.manageNickname("nick", nullptr, CHECK)) { ... }
+// server.manageNickname("nick", client, REGISTER);
+// server.manageNickname("nick", nullptr, UNREGISTER);
+
 
 Channel* Server::createOrGetChannel(const string& channelName)
 {
@@ -57,14 +166,14 @@ const string& Server::getPassword() const
     return _password;
 }
 
-const map<int, Users*>& Server::getUser() const
+const map<int, Clients*>& Server::getClient() const
 {
-    return _users;
+    return _Clients;
 }
 
-Users* Server::getUserByNickname(const string& nickname) const
+Clients* Server::getClientByNickname(const string& nickname) const
 {
-    for (map<int, Users*>::const_iterator it = _users.begin(); it != _users.end(); ++it)
+    for (map<int, Clients*>::const_iterator it = _Clients.begin(); it != _Clients.end(); ++it)
     {
         if (it->second->getNickname() == nickname)
             return it->second;
